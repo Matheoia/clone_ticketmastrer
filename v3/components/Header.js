650ff7ca68678fe62c2042305cbcc247bd99c8ca
Matheoia <121936719+Matheoia@users.noreply.github.com>
2023-10-10 08:27:09 +0200

    Add files via upload

---
+++ v3/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.css"
+import { Link } from "react-router-dom"
+
+function Header() {
+    return(
+        <header>
+            <div className="navigation">
+                <Link to="/"><img id="logoNBA" src="https://3.bp.blogspot.com/-ATl-uSY6l0E/XoCe1-kY4tI/AAAAAAAAZEw/K_GL6FQkngoZcmEKXxLD_RryoNl7hv0CACLcBGAsYHQ/s1600/NBA%2Blogo-02.png"></img></Link>
+                <Link className="navBar--teams" to="/teams"><p>Teams</p></Link>
+                <Link className="navBar--games" to="/games"><p>Games</p></Link>
+            </div>
+        </header>
+        
+    )
+}
+
+export default Header
---
+++ v3/components/TicketSection.js
@@ -0,0 +1,62 @@
+import React, { createElement } from "react"
+import { useEffect, useState } from "react"
+import ApiNBATickets from "../ApiNBATickets";
+import "./TicketSection.css"
+import { Link } from "react-router-dom";
+
+function TicketSection() {
+
+    const [matchesList, SetMatchesList] = useState([])
+    useEffect(() => {
+        const loadAllMatches = async () => {
+          try {
+            let list = await ApiNBATickets.getNBATickets();
+            SetMatchesList(list);
+            console.log(list);
+          } catch (error) {
+            //console.error('Erreur lors du chargement des matchs NBA :', error);
+          }
+        };
+    
+        loadAllMatches();
+      }, []);
+    
+
+    const handleMatchClick = async (item) => {
+        console.log(item);
+        // window.location.href = '/games/reservation'
+
+        
+    }
+
+    return (
+        
+
+        matchesList.map((games, i) => (
+            games.items.length > 0 && (
+                <div className="matchRow">
+                <h2>{games.title}</h2>
+                <div className="matchRow--list">
+                    {games.items.length > 0 && 
+                    games.items.map((item, key) => (
+                        item._embedded && (
+                            <div className="matchRow--item" key={key}>
+                                <Link to={`/games/${item.id}/reservation`} className="matchRow--info">
+                                    <img alt={item.name} src={item._embedded.attractions[0].images[0].url} style={{ width: '200px', height: '133px' }} />
+                                    <label>VS</label>
+                                    <img alt={item.name} src={item._embedded.attractions[1].images[0].url} style={{ width: '200px', height: '133px' }} />
+                                </Link>
+                                <p><label>{item._embedded.attractions[0].name}</label><label>{item.dates.start.localDate}</label><label>{item.dates.start.localTime}</label><label>{item._embedded.attractions[1].name}</label></p>
+                            </div>
+                            
+                        )
+                    ))}
+                </div>
+            </div>
+
+            )
+        ))
+    )
+}
+
+export default TicketSection
